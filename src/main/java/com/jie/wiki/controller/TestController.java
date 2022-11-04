2dceb4eb73ffbfb93260ef29a2dbe53dcac96676
pengxinjie <1370639979@qq.com>
2022-11-04 11:10:00 +0800

    idea集成mysql

--- src/main/java/com/jie/wiki/controller/TestController.java
+++ src/main/java/com/jie/wiki/controller/TestController.java
@@ -1,13 +1,9 @@
 package com.jie.wiki.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.Map;
 
 /**
  * @author Pengxinjie
@@ -15,6 +11,11 @@ import java.util.Map;
  */
 @RestController //返回json字符串，@Controller: ：返回页面
 public class TestController {
+
+    // @Value注解：导入自定义的配置项，：default，默认配置（如果没有配置）
+    @Value("${test.myname:default}")
+    private String MyName;
+
     @GetMapping("/hello")
     public String hello(){
         return "hello world";
@@ -22,6 +23,6 @@ public class TestController {
 
     @PostMapping("/hello/post")
     public String helloPost(String name){
-        return "hello "+name;
+        return "hello "+name+",my name is "+MyName+".";
     }
 }
